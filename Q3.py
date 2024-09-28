9034402d9a2afebb568be50dd3bae607b9253622
aileesant <166937749+aileesant@users.noreply.github.com>
2024-09-28 11:41:31 -0300

    Q3.py

--- Q3.py
+++ Q3.py
@@ -17,8 +17,9 @@ print(f'o total dos números digitado foi: {total}')
 list = []
 
 for i in range(10): 
-    valor = int(input(f'Digite um número: {i+1}'))
+    valor = int(input(f'Digite o valor {i+1}: '))
     list.append(valor)
 
 print(f'os números digitados foram: {list}')
-
+print(f'o maior número foi: {sum(list)}')
+print(f'a soma dos números foi: {sum(list)}')
