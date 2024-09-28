1f40b142d2773c56a07c5415abf76b3d3f39124a
aileesant <166937749+aileesant@users.noreply.github.com>
2024-09-28 11:32:58 -0300

    Q3.py

--- Q3.py
+++ Q3.py
@@ -10,4 +10,15 @@ maior_valor = max(list)
 print(f'o maior número digitado foi: {maior_valor}')
 
 total = sum(list)  
-print(f'o totl dos números digitado foi: {total}')
+print(f'o total dos números digitado foi: {total}')
+
+-----------------------------------------------------------------
+
+list = []
+
+for i in range(10): 
+    valor = int(input(f'Digite um número: {i+1}'))
+    list.append(valor)
+
+print(f'os números digitados foram: {list}')
+
