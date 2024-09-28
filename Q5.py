626d90a26c45ba48e3aacb2d7de6e9a91936fed6
aileesant <166937749+aileesant@users.noreply.github.com>
2024-09-28 11:57:03 -0300

    Q5.py

--- Q5.py
+++ Q5.py
@@ -5,7 +5,7 @@ for i in range(5):
     numeros.append(valor)
 
 media = sum(numeros) / len(numeros)
-print(f'A média dos números da lista: {media:.f}') 
+print(f'A média dos números da lista: {media:.2f}') 
 
 #esse {media:.f} serve para tirar as dízimas periódicas 
 # deixa com apena 2 dígitos 
